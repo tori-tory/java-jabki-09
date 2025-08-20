a1e7cddf4d14f2ffa72d335e56257879fa1be766
tori-tory <molkovaelena@rambler.ru>
2025-08-20 17:56:25 +0300

    lesson 09 - исправление по замечаниям №4

--- src/MainTest.java
+++ src/MainTest.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 class MainTest {
     @Test
-    public void testMain() {
+    public void testAreAnagram() {
         Assertions.assertTrue(Main.areAnagram("банка", "кабан"));
         Assertions.assertTrue(Main.areAnagram("мышка", "камыш"));
         Assertions.assertFalse(Main.areAnagram("ааа", "ооо"));
+    }
 
+    @Test void hasDuplicates() {
         Assertions.assertTrue(Main.hasDuplicates(new ArrayList<>(List.of(1, 2, 3, 4, 2, 3))));
         Assertions.assertFalse(Main.hasDuplicates(new ArrayList<>(List.of(1, 2, 3, 4))));
     }
